b0f6fd9675539746119d006e4ea61c7e57611d3e
Timothy Arlet <tarlet@cnm.edu>
2024-01-29 13:40:45 -0700

    Completed the trick.

--- src/main/java/edu/cnm/deepdive/cards/model/Deck.java
+++ src/main/java/edu/cnm/deepdive/cards/model/Deck.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.cards.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.random.RandomGenerator;
 
@@ -26,6 +27,23 @@ public class Deck {
 
   public void sort() {
     cards.sort(null);
+  }
+
+  public int size() {
+    return cards.size();
+  }
+
+  public boolean isEmpty() {
+    return cards.isEmpty();
+  }
+
+  /**
+   * Draws a single {@link Card} instance from the deck.
+   * @return instance of {@link Card}.
+   * @throws NoSuchElementException if deck is empty.
+   */
+  public Card draw() throws NoSuchElementException {
+    return cards.removeFirst();
   }
 
   @Override
